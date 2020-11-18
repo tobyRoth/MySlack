a9531db3f8b48378187993da5ffc8707228cb98c
tobyRoth <tobyroth18@gmail.com>
2020-11-18 19:19:01 +0200

    my slck

---
+++ scripts/Functions.js
@@ -0,0 +1,100 @@
+//---functions---//
+//post message
+function addMessage(channel,msg){
+    return new Promise(function(resolve,reject){
+         slack.api('chat.postMessage', {
+         text:msg,
+         channel:channel,
+     }, function(err, response){
+         console.log("post!")
+         console.log(response);
+         resolve(response);
+     });
+ });
+ }
+ //get all messages
+ function getAllMessages(id){
+     return new Promise(function(resolve, reject){
+         slack.api('conversations.history', {
+             token: apiToken,
+             channel:id
+         },function (err, response) {
+                     console.log("get!");
+                     console.log('response:');
+                     console.log(response.messages);
+                     resolve(response.messages)
+         });
+     });
+ } 
+ //get all channels
+ function getAllChannels(){
+     return new Promise((function(resolve, reject){
+         slack.api('conversations.list', {
+             token: apiToken
+         },function (err, response) {
+                 console.log('list:');
+                 console.log(response.ok);
+                 resolve(response)
+     });
+ }))
+ }
+ //serch massege
+ function searchMessage(msg){
+     return new Promise(function(resolve,reject){
+          slack.api('search.messages', {
+          token:apiToken,
+          query:msg
+      }, function(err, response){
+          resolve(response);
+      });
+  });
+ }
+ //---help functions to store response data---//
+ let messagesList=[];
+ //all messages
+ function resMessages(result){ 
+     for(var i=0;i<result.length;i++){
+         messagesList.push(
+             {
+                 'text':result[i].text,
+                 'user id':result[i].user,
+                 'time stamp':result[i].ts
+             }
+             );
+     }
+     return messagesList;
+         
+ } 
+ //all channels
+ let conversations=[];
+ function resChannels(channels) {
+     
+     for(var i=0;i<channels.length;i++)
+         {
+             conversations.push(
+                 {
+                     'channel name':channels[i].name,
+                     'channel id':channels[i].id
+                 }
+             )   
+         }
+        return conversations;
+ }
+ let matches=[];
+ //all matches messages to search
+ function resMatches(messages){
+         console.log(messages.matches.length);
+         for(var i=0;i<messages.matches.length;i++)
+         {
+             matches.push(
+                 {
+                     'user name':messages.matches[i].username,
+                     'text':messages.matches[i].text,
+                     'perma link':messages.matches[i].permalink,
+                     'time stamp':messages.matches[i].ts
+                 }
+             )   
+         }
+         return matches;
+ }
+ module.exports = { addMessage,getAllMessages,getAllChannels,searchMessage,resMessages,resChannels,resMatches }
---
+++ scripts/SlackApp.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const Functions = require('./Functions');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+var Slack = require('slack-node');
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 5000;
+}
+app.listen(port);
+
+//---routing---//
+
+app.get('/', function(req, res){
+  });
+//add message
+app.post('/add-message', async(req, res) => {
+    try{
+        
+        apiToken =req.query.token;
+        if(!apiToken)
+            return res.send({'status':'failed, token is requierd'})
+        slack = new Slack(apiToken);
+        var channel=req.query.channel;
+        if(!channel)
+            return res.send({'status':'failed, channel is required'})
+        var msg=req.query.msg;
+        if(!msg)
+            return res.send({'status':'failed, messege is required'})
+        Functions.addMessage(channel,msg).then(function(result){
+            if(!result.ok)
+                return res.send({'status':result});
+            res.send({"u just posted a message":result.message.text});
+        });
+    }
+    catch(error){
+        console.error(error);    
+    }
+});
+//get all messages from a specific channel
+app.get('/get-all-messages', async(req, res) => {
+    try{
+        apiToken =req.query.token;
+        if(!apiToken)
+            return res.send({'status':'failed, token is required'});
+        slack = new Slack(apiToken);
+        var channel=req.query.channel;
+        if(!channel)
+            return res.send({'status':'failed, channel is required'})
+        Functions.getAllMessages(channel).then(function(result){
+            if(!result)
+                return res.send({'status':'failed, token or channel not found'});
+            let messagesList=Functions.resMessages(result);
+            res.send({"all your messages":messagesList});
+        });
+    }
+    catch(error){
+        console.log(console.error());
+    }    
+});
+//get all channels(name and id)
+app.get('/get-all-channels', async(req, res) => {
+    try{
+        apiToken =req.query.token;
+        if(!apiToken)
+            return res.send({'status':'failed, token is required'});
+        slack = new Slack(apiToken);
+        Functions.getAllChannels().then(function(result){
+        if(!result.ok)
+            return res.send({'status':result});
+            let conversations=Functions.resChannels(result.channels);
+        res.send({'all channels:':conversations});
+        });
+    }
+    catch(error){
+        console.error(error);
+    }    
+});
+//search for a message in all channels
+app.get('/search-message', async(req, res) => {
+    try{
+        apiToken =req.query.token;
+        if(!apiToken)
+            return res.send({'status':'failed, token is required'});
+        slack = new Slack(apiToken);
+        var msg=req.query.msg;
+        if(!msg)
+            return res.send({'status':'failed, messege is required'});
+        Functions.searchMessage(msg).then(function(result){
+        console.log(result)
+        if(!result.ok)
+            return res.send({'status':result});
+        let matches=Functions.resMatches(result.messages); 
+        if(matches.length>0)             
+            res.send({'messages found':matches});
+        res.send('no matches messages found:(');
+        });
+    }
+    catch(error){
+        console.log(console.error);
+    }    
+});
+app.get('*', function(req, res){
+    res.status(404).send('page was not found:(');
+  });
+
